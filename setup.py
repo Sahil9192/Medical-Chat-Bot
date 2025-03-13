6eaa4e1ad4547c8df5ca6d08a59819fc13eab1e9
Sahil9192 <sahilypawar9192@gmail.com>
2025-03-13 22:04:48 +0530

    Initial commit - Setup project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Sahil Pawar',
+    author_email = 'sahilypawar9192@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
